181aff0178c6f4ab1f0f81f8239c4153ef43094a
tommasogangemi <tommaso.gangemi@kampaay.com>
2023-04-30 11:27:07 +0200

    feat(auth): added encoded keys and exposed token endpoint

--- index.ts
+++ index.ts
@@ -1,20 +1,25 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import { CLIENT_EMAIL, PRIVATE_KEY, PRIVATE_KEY_ID, decodeKey } from './keys'
 const google = require('googleapis')
 
 dotenv.config()
 
+// configure a JWT auth client
+let jwtClient = new google.Auth.JWT({
+  key: decodeKey(PRIVATE_KEY),
+  keyId: decodeKey(PRIVATE_KEY_ID),
+  email: decodeKey(CLIENT_EMAIL),
+  scopes: 'https://www.googleapis.com/auth/spreadsheets'
+})
+
 const app: Express = express()
-const port = process.env.PORT
+const port = process.env.NODE_ENV === 'development' ? 5000 : ''
 
-app.get('/', async (req: Request, res: Response) => {
-  // const creds = await jwtClient.authorize()
+app.get('/token', async (req: Request, res: Response) => {
+  const authorization = await jwtClient.authorize()
 
-  // console.log('aaaaaaaaaaaa', creds)
-
-  // res.send(JSON.stringify(creds, undefined, 4))
-
-  res.send('Hello World!')
+  res.send(JSON.stringify(authorization))
 })
 
 app.listen(port, () => {
